ca96819170aa1b32ea7b02200bd68c59276be3cf
GermanKuchen <germankuchen@gmail.com>
2019-08-22 15:36:08 -0300

    Nuevo

--- src/app/components/virtual/virtual.component.ts
+++ src/app/components/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild( CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
+
   personas = new Array(500).fill(0);
 
   constructor() {
@@ -14,6 +17,14 @@ export class VirtualComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  irAlFinal() {
+    this.viewport.scrollToIndex(this.personas.length);
+  }
+
+  irAlInicio() {
+    this.viewport.scrollToIndex(0);
   }
 
 }
