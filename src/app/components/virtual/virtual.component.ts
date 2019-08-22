2fd1114bfebf36d90ac64329e4562423b2513613
GermanKuchen <germankuchen@gmail.com>
2019-08-22 14:40:54 -0300

    Lo ultimo

---
+++ src/app/components/virtual/virtual.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+  personas = new Array(500).fill(0);
+
+  constructor() {
+    console.log(this.personas);
+   }
+
+  ngOnInit() {
+  }
+
+}
